c7c6cdab1d5db6f77492b5aeaa5cc46a55497675
Brooklyn-Dev <brooklynbaylisdev@gmail.com>
2024-09-07 13:44:26 +0100

    Implement screen edge avoidance
    feat: implement ability for boids to avoid the edges of the screen by making them smoothly turn in the opposite direction when they cross a certain threshold

--- src/boid.py
+++ src/boid.py
@@ -3,16 +3,18 @@ from typing import Tuple
 
 import pygame as pg
 
-from settings import MAX_SPEED, MIN_SPEED
+from settings import MAX_SPEED, MIN_SPEED, SCREEN_MARGIN_LEFT, SCREEN_MARGIN_RIGHT, SCREEN_MARGIN_TOP, SCREEN_MARGIN_BOTTOM, TURN_SPEED
 
 class Boid:
     def __init__(self, size: float, position: pg.Vector2, velocity: pg.Vector2) -> None:
-        self.size = abs(size)
+        self.size: float = abs(size)
         
         self.position: pg.Vector2 = position
-        self.velocity: pg.Vector2 = velocity
+        self.velocity: pg.Vector2 = velocity.clamp_magnitude(MIN_SPEED, MAX_SPEED)
 
     def update(self) -> None:
+        self._avoid_screen_edges()
+        
         self.position += self.velocity
         
     def draw(self, screen: pg.Surface) -> None:
@@ -29,3 +31,13 @@ class Boid:
     def apply_force(self, force: pg.Vector2) -> None:
         self.velocity += force
         self.velocity.clamp_magnitude_ip(MIN_SPEED, MAX_SPEED)
+        
+    def _avoid_screen_edges(self) -> None:
+        if self.position.x < SCREEN_MARGIN_LEFT:
+            self.apply_force(pg.Vector2(TURN_SPEED, 0))
+        if self.position.x > SCREEN_MARGIN_RIGHT:
+            self.apply_force(pg.Vector2(-TURN_SPEED, 0))
+        if self.position.y < SCREEN_MARGIN_TOP:
+            self.apply_force(pg.Vector2(0, TURN_SPEED))
+        if self.position.y > SCREEN_MARGIN_BOTTOM:
+            self.apply_force(pg.Vector2(0, -TURN_SPEED))
